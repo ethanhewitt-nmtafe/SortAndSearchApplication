7ba4547365d798b9d6ca37cf047719af2a46f151
20098904 <20098904@tafe.wa.edu.au>
2026-03-27 10:13:13 +0800

    Completed Task Three

--- SortAndSearchApplication/Program.cs
+++ SortAndSearchApplication/Program.cs
@@ -1,5 +1,10 @@
-﻿// 4 Numbers | ranges from 1 - 45
+﻿// 4 Numbers | ranges from 1 - 20
 int totalOfNumbers = 4;
+int lowestValue = 1;
+int highestValue = 20;
+
+// User Inputs Numbers
+
 int[] userNumbers = new int[totalOfNumbers];
 
 for (int i = 0; i < userNumbers.Length; i++)
@@ -8,11 +13,39 @@ for (int i = 0; i < userNumbers.Length; i++)
     string userInput = Console.ReadLine();
     if (int.TryParse(userInput, out int number))
     {
-        userNumbers[i] = number;
+        if(number >= lowestValue &&  number <= highestValue)
+        {
+            userNumbers[i] = number;
+        }
+        else
+        {
+            Console.WriteLine("Please Enter A Valid Number");
+            i -= 1;
+
+        }
+
     }
     else
     {
         Console.WriteLine("Please Enter A Valid Number");
         i -= 1;
     }
+}
+
+// Random Number Generator
+
+Random rnd = new Random();
+
+int[] computerNumbers = new int[totalOfNumbers];
+
+
+for (int i = 0; i < totalOfNumbers; i++)
+{
+    int randomNumber = rnd.Next(lowestValue, highestValue);
+    computerNumbers[i] = randomNumber;
+}
+
+foreach (int computerNumber in computerNumbers)
+{
+    Console.WriteLine(computerNumber);
 }
